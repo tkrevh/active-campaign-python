b4664b101c9d298d15e6627e5650781ea6199741
Dennis Durling <djdtahoe@gmail.com>
2017-09-21 12:00:06 -0700

    lets go

--- setup.py
+++ setup.py
@@ -1,43 +1,49 @@
-from setuptools import setup
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-desc = open("readme.rst").read() if os.path.isfile("readme.rst") else ""
 
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+finally:
+    import os
+
+readme = open("README.md").read() if os.path.isfile("README.md") else ""
+
+requirements = [
+    'requests'
+]
+
+test_requirements = [
+    # none yet
+]
 
 setup(
-    name='flask_datatables',
-    version='0.6.17',
-    packages=['flask_datatables', 'flask_datatables.views'],
-    url='https://github.com/tahoe/flask_datatables/',
-    download_url='https://github.com/tahoe/flask_datatables/tarball/0.6.15',
-    license='MIT',
-    long_description=desc,
-    keywords='datatables sqlalchemy flask',
-    author='Dennis Durling',
+    name='active-campaign-python',
+    version='0.5.0',
+    description="Python ActiveCampaign API client",
+    long_description=readme,
+    author="Dennis Durling",
     author_email='djdtahoe@gmail.com',
-    description='Integrates SQLAlchemy with DataTables (framework Flask)',
-    zip_safe=False,
+    url='https://github.com/tahoe/active-campaign-python',
+    packages=[ 'activecampaign', ],
+    package_dir={'activecampaign': 'activecampaign'},
     include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='activecampaign',
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Flask',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    install_requires=[
-        'querystring_parser==1.2.3',
-        'sqlalchemy>=1.0.11',
-        'flask==0.10.1',
-        'flask-restful>=0.3.5',
-        'Faker==0.7.12',
-    ],
+    ]
 )
 
