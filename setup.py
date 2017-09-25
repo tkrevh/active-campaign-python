0bc4073b61cd98b268fc5417fcc5cc7b67dd77a4
Dennis Durling <djdtahoe@gmail.com>
2017-09-24 22:41:46 -0700

    moved shortcut commands to main object

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='active-campaign-python',
-    version='0.7.3',
+    version='0.7.4',
     description="Python ActiveCampaign API client",
     long_description=readme,
     author="Dennis Durling",
